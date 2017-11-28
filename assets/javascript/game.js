777f33dfdf004a19f2ec79f7b0ca8b7066c2e65d
Mayra Jaimes <mtj26@georgetown.edu>
2017-11-28 00:31:58 -0600

    I added html content and some variables to begin with

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var words = ["doctor", "teacher", "police", "firefighter"];
+var currentWord = words[Math.floor(Math.random() * words.length)];
+
+var guessRemain = 15;
+var lettGuess = 0;
+var numWin = 0;
+
+var underScore = 0;
+
