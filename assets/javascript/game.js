69d4cd8c67706d27101cf5c2a89264ccefbc43ac
Mayra Jaimes <mtj26@georgetown.edu>
2017-11-28 22:38:37 -0600

    I added pseudocode notes and broke down the game in different steps. I also added some variables for each step

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,48 @@
+// Create words
+
 var words = ["doctor", "teacher", "police", "firefighter"];
+
+//Pick random word from the array
+
 var currentWord = words[Math.floor(Math.random() * words.length)];
 
-var guessRemain = 15;
-var lettGuess = 0;
-var numWin = 0;
+//Create array of underscores equal to the number of letters
 
-var underScore = 0;
+var lettersArray = [];
+
+for (var i = 0; i < currentWord.length; i++) {
+    lettersArray[i] = "_ ";
+	}
+
+document.getElementById("lettersArray").textContent = lettersArray;
+
+
+// This function is run whenever the user presses a key. Then its determined which key was pressed.
+document.onkeyup = function(event) {
+var userGuess = event.key; }
+
+
+//IF correct add the letter to the array and decrease the remaining letters number.
+//IF incorrect add the userGuess to the wrongGuessArray and decrease the remaining tries number. 
+
+var remainLetters = word.length;
+var wrongGuessArray = [];
+var remainTries = 15;
+
+	for (var j = 0; j < currentWord.length; j++) {
+
+		if (currentWord[j] === userGuess) {
+	    letterArray[j] = useGuess;
+		remainLetters--;
+	    }
+
+	    else {
+	    wrongGuessArray.push(userGuess);
+	    remainTries--;
+	    }
+  }
+
+document.getElementById("remainLetters").textContent = remainLetters;
+document.getElementById("wrongGuessArray").textContent = wrongGuessArray;
+document.getElementById("remainTries").textContent = remainTries;
 
