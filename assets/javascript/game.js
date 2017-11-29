9ad8d35a38fa79cc48ac8cec46cfba3fa7fce685
Mayra Jaimes <mtj26@georgetown.edu>
2017-11-29 13:58:33 -0600

    added functions for resets when player wins or losses and functions for generating underscore and new random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,108 +12,98 @@ var rightGuessArray = [];
 
 var userGuess;
 
-//Picks random word from the words array
+var randomWord = words [Math.floor(Math.random() * words.length)];
 
+//List of Functions:
 
-function resetNewWord(){
+function generateWord() {
+	var randomWord = words [Math.floor(Math.random() * words.length)];
+};
+
+function generateUnderscore () {
+	for (var i = 0; i < randomWord.length; i++) {
+		answerArray.push('_');
+		document.getElementById("remainTries").textContent = remainTries;
+	}
+	document.getElementById("answerArray").textContent = answerArray.join(" ");
+};
+
+function resetNextGame() {
 	document.onkeyup = function(event) {
-	var userGuess = event.key;
+	userGuess = event.key;
 	if (event.keyCode = 12){
+		wrongGuessArray = [];
+		document.getElementById("wrongGuessArray").textContent = wrongGuessArray;
+		rightGuessArray = [];
+		document.getElementById("rightGuessArray").textContent = rightGuessArray;
+		answerArray = [];
+		document.getElementById("answerArray").textContent = answerArray;
 		remainTries = 10;
 		document.getElementById("remainTries").textContent = remainTries;
-		outcome = "";
+		outcome = " ";
 		document.getElementById("outcome").textContent = outcome;
 		}
 	}
-}
+};
 
-function resetNewGame(){
+function startGame(){
+	generateWord();
+	generateUnderscore();
 
-}
+	document.onkeyup = function(event) {
+	userGuess = event.key;
+		if (event.keyCode >= 65 && event.keyCode <= 90){
 
-var randomWord = words [Math.floor(Math.random() * words.length)];
+			//Conditional statements for right and wrong guesses:
+			if (randomWord.indexOf(userGuess) > -1) {
+				for (var j = 0; j < randomWord.length; j++){
+					if (randomWord[j] === userGuess) {
+						answerArray[j] = userGuess;	
+					}
+				}
+			document.getElementById("answerArray").textContent = answerArray.join(" ");
+			rightGuessArray.push(userGuess);
+			document.getElementById("rightGuessArray").textContent = rightGuessArray.join(" ");
 
-for (var i = 0; i < randomWord.length; i++) {
-	    answerArray.push('_');
-	    document.getElementById("remainTries").textContent = remainTries;
-	}
+			}
+			else {
+				wrongGuessArray.push(userGuess);
+				document.getElementById("wrongGuessArray").textContent = wrongGuessArray.join(" ");
+				remainTries --;
+				document.getElementById("remainTries").textContent = remainTries;
+			}
 
-		document.getElementById("answerArray").textContent = answerArray.join(" ");
+			//Conditional statements for win and loss games:
+			if (answerArray.join("") === randomWord) {
+				numWins ++;
+				document.getElementById("numWins").textContent = numWins;
+		        outcome = "You won!";
+		        document.getElementById("outcome").textContent = outcome;
+		        resetNextGame();
+			}
 
-// Determines which key was pressed.
-document.onkeyup = function(event) {
-	var userGuess = event.key;
-	if (event.keyCode >= 65 && event.keyCode <= 90){
-
-
-//Conditional statements for right and wrong guesses:
-
-	if (randomWord.indexOf(userGuess) > -1) {
-		for (var j = 0; j < randomWord.length; j++){
-			if (randomWord[j] === userGuess) {
-				answerArray[j] = userGuess;	
+			else if (remainTries === 0) {
+				numLosses ++;
+		        outcome = "You lost!";
+		        document.getElementById("numLosses").textContent = numLosses;
+		        document.getElementById("outcome").textContent = outcome;
+		        resetNextGame();
+		    }
+		else {
+			return;
 			}
 		}
-	document.getElementById("answerArray").textContent = answerArray.join(" ");
-	rightGuessArray.push(userGuess);
-	document.getElementById("rightGuessArray").textContent = rightGuessArray.join(" ");
-	}
-
-	else {
-		wrongGuessArray.push(userGuess);
-		document.getElementById("wrongGuessArray").textContent = wrongGuessArray.join(" ");
-		remainTries --;
-		document.getElementById("remainTries").textContent = remainTries;
-	}
-
-//Conditional statements for win and loss games:
-	if (answerArray.join("") === randomWord) {
-		numWins ++;
-		document.getElementById("numWins").textContent = numWins;
-        outcome = "You won!";
-        document.getElementById("outcome").textContent = outcome;
-        resetNewWord();
-	}
-
-	else if (remainTries === 0) {
-		numLosses ++;
-        outcome = "You lost!";
-        document.getElementById("numLosses").textContent = numLosses;
-        document.getElementById("outcome").textContent = outcome;
-        resetNewWord();
-    }
-
-	else {
-		return;
 	}
 }
-}
 
- 
-
-
-//Conditional statements for right and wrong guesses:
-
-
-
-
-
-
-
+startGame();
 
 
 /*
 	//After win or lose pick a new word display play new game button next to reset button
 
-	//When play new game is pressed, reset 
+	//Start Over: Make function that resets everythnig. 
 
-	//IF click reset everything: numWins, numLosses, remainLetters, wrongGuessArray, remainTries, letters array, 
-	var reset = 0;
-
-	while (remainingLetters > 0) {
-	}
-}
 
 */
-
 
