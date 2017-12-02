0ff860686ee7b299c0d969e3aff6b5c1b9bc7286
Mayra Jaimes <mtj26@georgetown.edu>
2017-12-01 21:42:59 -0600

    made my javascript object oriented

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,108 +1,106 @@
-//Global variables
-var d = document;
-var numWins = 0;
-var numLosses = 0;
-var remainTries = 10;
-var outcome;
-var currentWordPos;
-var userGuess;
-var randomWord;
-var gameOver = false;
+var hangManGame = {
+	numWins: 0,
+	numLosses: 0,
+	remainTries: 10,
+	outcome: "",
+	currentWordPos: "",
+	userGuess: "",
+	randomWord: "",
+	gameOver: false,
+	words: ["BASKETBALL", "SOCCER", "BICYCLING", "SWIMMING", "GOLF", "BOWLING", "WRESTLING", "TENNIS", "KAYAKING", "HOCKEY"],
+	answerArray: [],
+	wrongGuessArray: [],
+	rightGuessArray: [],
+	wrongGuessContent: document.getElementById("wrongGuessContent"),
+	rightGuessContent: document.getElementById("rightGuessContent"),
+	answerContent: document.getElementById("answerContent"),
+	remainTriesContent: document.getElementById("remainTriesContent"),
+	outcomeContent: document.getElementById("outcomeContent"),
+	numWinsContent: document.getElementById("numWinsContent"),
+	numLossesContent: document.getElementById("numLossesContent"),	
 
-//Arrays
-var words = ["DOCTOR", "TEACHER", "POLICE", "NURSE"];
-var answerArray = [];
-var wrongGuessArray = [];
-var rightGuessArray = [];
+	generateWord: function() {
+	  var wordPos = Math.floor(Math.random() * this.words.length);
+	  if (this.currentWordPos === wordPos) {
+	    generateWord();
+	  } else {
+	    this.currentWordPos = wordPos;
+	    this.randomWord = this.words[wordPos];
+	  }
+	},
 
-//Variables to get Elements
-var wrongGuessContent = d.getElementById("wrongGuessContent");
-var rightGuessContent = d.getElementById("rightGuessContent");
-var answerContent = d.getElementById("answerContent");
-var remainTriesContent = d.getElementById("remainTriesContent");
-var outcomeContent = d.getElementById("outcomeContent");
-var numWinsContent = d.getElementById("numWinsContent");
-var numLossesContent = d.getElementById("numLossesContent")
-
-//Functions:
-function generateWord() {
-  var wordPos = Math.floor(Math.random() * words.length);
-  if (currentWordPos === wordPos) {
-    generateWord();
-  } else {
-    currentWordPos = wordPos;
-    randomWord = words[wordPos];
-  }
-};
-
-function generateUnderscore () {
-	for (var i = 0; i < randomWord.length; i++) {
-		answerArray.push('_');
-	}
-	answerContent.textContent = answerArray.join(" ");
-};
-
-function resetNextGame() {
-	gameOver = false;	
-	wrongGuessArray = [];
-	rightGuessArray = [];
-	answerArray = [];
-	remainTries = 10;
-	outcome = " ";
-	wrongGuessContent.textContent = wrongGuessArray;
-	rightGuessContent.textContent = rightGuessArray;
-	answerContent.textContent = answerArray;
-	remainTriesContent.textContent = remainTries;
-	outcomeContent.textContent = outcome;
-};
-
-function startGame(){
-	generateWord();
-	generateUnderscore();
-
-	document.onkeyup = function(event) {
-	userGuess = event.key.toUpperCase();
-
-		if (event.keyCode === 83 && gameOver == true){
-			resetNextGame();
-			startGame();
+	generateUnderscore: function() {
+		for (var i = 0; i < this.randomWord.length; i++) {
+			this.answerArray.push('_');
 		}
-		
-		else if (event.keyCode >= 65 && event.keyCode <= 90 && gameOver == false){
+		this.answerContent.textContent = this.answerArray.join(" ");
+	},
 
-			//Conditional statements for right and wrong guesses:
-			if (randomWord.indexOf(userGuess) > -1) {
-				for (var j = 0; j < randomWord.length; j++){
-					if (randomWord[j] === userGuess) {
-						answerArray[j] = userGuess;	
-					}
-				}
-				rightGuessArray.push(userGuess);
-				answerContent.textContent = answerArray.join(" ");
-				rightGuessContent.textContent = rightGuessArray.join(" ");
+	resetNextGame: function() {
+		this.gameOver = false;	
+		this.wrongGuessArray = [];
+		this.rightGuessArray = [];
+		this.answerArray = [];
+		this.remainTries = 10;
+		this.outcome = " ";
+		this.wrongGuessContent.textContent = this.wrongGuessArray;
+		this.rightGuessContent.textContent = this.rightGuessArray;
+		this.answerContent.textContent = this.answerArray;
+		this.remainTriesContent.textContent = this.remainTries;
+		this.outcomeContent.textContent = this.outcome;
+	},
 
-			} else {
-				wrongGuessArray.push(userGuess);
-				remainTries--;
-				wrongGuessContent.textContent = wrongGuessArray.join(" ");
-				remainTriesContent.textContent = remainTries;
+	startGame: function() {
+		console.log('startGame');
+		this.generateWord();
+		this.generateUnderscore();
+
+		var thisGame = this;
+
+		document.onkeyup = function(event) {
+			this.userGuess = event.key.toUpperCase();
+			
+			if (event.keyCode === 83 && this.gameOver == true){
+				this.resetNextGame();
+				this.startGame();
 			}
-			//Conditional statements for win and loss games:
-			if (answerArray.join("") === randomWord) {
-				numWins ++;
-		        outcome = 'YOU WON! Press "s" to play another word';
-		        gameOver = true;
-		        numWinsContent.textContent = numWins;
-		        outcomeContent.textContent = outcome;
-			} else if (remainTries === 0) {
-				numLosses ++;
-		        outcome = 'YOU LOST! Press "s" to play another word';
-		        gameOver = true;
-		        numLossesContent.textContent = numLosses;
-		        outcomeContent.textContent = outcome;
-		    }
-		}
-	}
-}
+			
+			else if (event.keyCode >= 65 && event.keyCode <= 90 && this.gameOver == false){
+				//Conditional statements for right and wrong guesses:
+				if (this.randomWord.indexOf(this.userGuess) > -1) {
+					for (var j = 0; j < this.randomWord.length; j++){
+						if (this.randomWord[j] === this.userGuess) {
+							this.answerArray[j] = this.userGuess;	
+						}
+					}
+					this.rightGuessArray.push(this.userGuess);
+					this.answerContent.textContent = this.answerArray.join(" ");
+					this.rightGuessContent.textContent = this.rightGuessArray.join(" ");
 
-startGame();
+				} else {
+					this.wrongGuessArray.push(this.userGuess);
+					this.remainTries--;
+					this.wrongGuessContent.textContent = this.wrongGuessArray.join(" ");
+					this.remainTriesContent.textContent = this.remainTries;
+				}
+				//Conditional statements for win and loss games:
+				if (this.answerArray.join("") === this.randomWord) {
+					this.numWins ++;
+			        this.outcome = 'YOU WON! Press "s" to play another word';
+			        this.gameOver = true;
+			        this.numWinsContent.textContent = this.numWins;
+			        this.outcomeContent.textContent = this.outcome;
+				} else if (this.remainTries === 0) {
+					this.numLosses ++;
+			        this.outcome = 'YOU LOST! Press "s" to play another word';
+			        this.gameOver = true;
+			        this.numLossesContent.textContent = this.numLosses;
+			        this.outcomeContent.textContent = this.outcome;
+			    }
+			}
+		}.bind(this)
+	}
+};
+
+hangManGame.startGame();
+
